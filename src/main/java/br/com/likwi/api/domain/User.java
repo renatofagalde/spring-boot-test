f880bbd4adf9d66ee708422091d31c4ab1c28d70
Renato Fagalde <renato@likwi.com.br>
2022-11-20 18:42:14 -0300

    classe usuário

---
+++ src/main/java/br/com/likwi/api/domain/User.java
@@ -0,0 +1,28 @@
+package br.com.likwi.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+@Entity(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+}
