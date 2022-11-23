f489abe5d4a031a0c0fddbe23ab20601f68263c7
Renato Fagalde <renato@likwi.com.br>
2022-11-23 17:41:32 -0300

    versão do java 11 para o deploy

--- src/main/java/br/com/likwi/api/controller/request/UserRequest.java
+++ src/main/java/br/com/likwi/api/controller/request/UserRequest.java
@@ -3,9 +3,11 @@ package br.com.likwi.api.controller.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserRequest {
 
     private Long id;
