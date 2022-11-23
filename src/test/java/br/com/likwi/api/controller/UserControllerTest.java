9b63133ed7f86d4706046dc533c47ab3640e1dee
Renato Fagalde <renato@likwi.com.br>
2022-11-22 21:39:04 -0300

    classe UserControllerTest.java com os métodos

---
+++ src/test/java/br/com/likwi/api/controller/UserControllerTest.java
@@ -0,0 +1,50 @@
+package br.com.likwi.api.controller;
+
+import br.com.likwi.api.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserControllerTest {
+
+    @InjectMocks
+    private UserController underTest;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
